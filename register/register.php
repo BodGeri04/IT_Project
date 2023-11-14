8299e2d792fb7d674e8303cc56adcf4dc53e5f53
kailuchen <150127017+kailuchen@users.noreply.github.com>
2023-11-14 18:05:11 +0800

    Add files via upload

--- register/register.php
+++ register/register.php
@@ -15,6 +15,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $mail = $_POST["mail"];
 
+// 基本的邮箱验证
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email address!";
+        mysqli_close($conn);
+        exit();
+    }
+
+
     // 首先检查邮箱地址是否已经存在|First check if the email address already exists
     $checkEmailQuery = "SELECT * FROM users WHERE email_address = '$mail'";
     $result = mysqli_query($conn, $checkEmailQuery);
@@ -24,6 +32,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         // 如果邮箱地址不存在，执行注册操作|If the email address does not exist, perform the registration operation
         $pwd = $_POST["pwd"];
+        // 密码至少8位数验证
+    if (strlen($pwd) < 8) {
+        echo "<div class='error-message'>The password needs to be at least 8 digits!</div>";
+        mysqli_close($conn);
+        exit();
+    }
 
         // Hash密码
         $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
@@ -41,3 +55,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 mysqli_close($conn);
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>REGUSTRATION</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>REGISTRATION</h1>
+        <form action="register.php" method="post">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name" required>
+            <label for="mail">Email Address:</label>
+            <input type="text" name="mail" id="mail" required>
+            <label for="pwd">Password:</label>
+            <input type="text" name="pwd" id="pwd" required>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+</body>
+</html>
