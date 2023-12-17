4a7f112618f8789e9e4c6ea1bd47ef816a330fff
Jeremy Beaulé <47953708+JeremyBeaule@users.noreply.github.com>
2023-12-18 00:59:36 +0100

    Add files via upload

--- IT_Project_/assets/drag_drop/nodeJs/Server.js
+++ IT_Project_/assets/drag_drop/nodeJs/Server.js
@@ -4,13 +4,13 @@ const con = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: '', // Assurez-vous que c'est le bon mot de passe
-    database: 'drag_and_drop_test',
+    database: 'it_project',
     charset: 'utf8mb4'
 });
 
 con.connect(function(err) {
     if (err) throw err;
-    console.log("Connecté à la base de données !");
+    //console.log("Connecté à la base de données !");
 });
 
 const express = require('express');
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.post('/save-stand', (req, res) => {
     const { ID, Event_ID, Color, Name, Rotation, Type, Width, Height, X_position, Y_position } = req.body;
     console
-    console.log("Reçu:", req.body); // Pour déboguer
+    //console.log("Reçu:", req.body); // Pour déboguer
     const sql = "INSERT INTO stands (ID, Event_ID, Color, Name, Rotation, Type, Width, Height, X_position, Y_position) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     con.query(sql, [ID, Event_ID, Color, Name, Rotation, Type, Width, Height, X_position, Y_position], function(err, result) {
         if (err) {
@@ -52,8 +52,55 @@ app.post('/clear-stands', (req, res) => {
     });
 });
 
+app.get('/getDimensions', (req, res) => {
+    // Récupérer l'ID depuis la requête GET
+    const eventId = req.query.eventId;
+
+    // Vérifier si l'ID est fourni
+    if (!eventId) {
+        return res.status(400).send('Aucun ID d\'événement fourni');
+    }
+
+    // Modifier la requête SQL pour filtrer en fonction de l'ID
+    const sql = 'SELECT market_height, market_width, small_height, small_width, medium_height, medium_width, large_height, large_width, id FROM events WHERE id = ?';
+
+    con.query(sql, [eventId], (err, result) => {
+        if (err) {
+            //console.log(err);
+            res.status(500).send('Erreur lors de la récupération des données');
+        } else {
+            //console.log(result);
+            res.json(result);
+        }
+    });
+});
+app.get('/getStands', (req, res) => {
+    // Récupérer l'ID depuis la requête GET
+    const eventId = req.query.eventId;
+    //console.log(eventId);
+
+    // Vérifier si l'ID est fourni
+    if (!eventId) {
+        return res.status(400).send('Aucun ID d\'événement fourni');
+    }
+
+    // Modifier la requête SQL pour sélectionner les champs spécifiques en fonction de l'ID
+    const sql = 'SELECT ID, Event_ID, Color, Name, Rotation, Type, Width, Height, X_position, Y_position FROM stands WHERE Event_ID = ?';
+
+    con.query(sql, [eventId], (err, result) => {
+        if (err) {
+            //console.log(err);
+            res.status(500).send('Erreur lors de la récupération des données');
+        } else {
+            //console.log(result);
+            res.json(result);
+        }
+    });
+});
+
+
 
 app.listen(3000, () => {
-    console.log('Serveur démarré sur le port 3000');
+    //console.log('Serveur démarré sur le port 3000');
 });
 
